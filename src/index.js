fa78a8e61f924266266a302d10baca73ca5c2d0a
SiGae <sigae@protonmail.com>
2020-08-17 17:04:38 +0900

    implement login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const conn = require('./dbconnection');
 
 const app = new Koa();
@@ -19,9 +20,31 @@ router.get('/idduplicate', async (ctx) => {
   ctx.body = { result: res.rowCount === 0 };
 });
 
+/*
+작성자 : SiGae
+일자 : 20/08/17
+설명 : db커넥션 생성 후 아이디 존재여부 확인후 패스워드 일치여부에 따라 true/false반환
+*/
+router.post('/loginmember', async (ctx) => {
+  const pg = conn.conn;
+  pg.connect();
+  const res = await pg.query(
+    `select password from userinfo where userId='${ctx.request.body.id}'`,
+  );
+  if (res.rowCount > 0) {
+    ctx.body = {
+      valid: true,
+      result: res.rows[0].password === ctx.request.body.password,
+    };
+  } else {
+    ctx.body = {
+      valid: false,
+    };
+  }
+});
+
+app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('running on port 4000');
-});
+app.listen(4000, () => {});
