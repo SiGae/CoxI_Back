f59f206d2f4157a6dd4344967f675f3fa0945fa0
SiGae <sigae@protonmail.com>
2020-08-24 17:13:27 +0900

    edit .gitignore and comment in dbconnection.js

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -1,6 +1,12 @@
 const { Client } = require('pg');
 const DbInfo = require('./dbconfig');
 
+/*
+작성자 : SiGae
+일자 : 20/08/14
+설명 : db커넥션모듈
+*/
+
 module.exports = {
   conn: new Client({
     user: DbInfo.id,
